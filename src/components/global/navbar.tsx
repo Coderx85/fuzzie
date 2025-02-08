227a19d7de66aa34d2db079737d65cf3b9b0b95b
Coderx85 <work.priyanshu085@gmail.com>
2025-02-08 19:59:29 +0530

    Upload src/component/global folder

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 import { MenuIcon } from 'lucide-react'
 // import { UserButton, currentUser } from '@clerk/nextjs'
 
-type Props = {}
+// type Props = {}
 
-const Navbar = async (props: Props) => {
+const Navbar = async () => {
   // const user = await currentUser()
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
@@ -22,26 +22,14 @@ const Navbar = async (props: Props) => {
         <p className="text-3xl font-bold">zie</p>
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
-        <ul className="flex items-center gap-4 list-none">
-          <li>
+        <div className="flex items-center gap-4 list-none">
             <Link href="#">Products</Link>
-          </li>
-          <li>
             <Link href="#">Pricing</Link>
-          </li>
-          <li>
             <Link href="#">Clients</Link>
-          </li>
-          <li>
             <Link href="#">Resources</Link>
-          </li>
-          <li>
             <Link href="#">Documentation</Link>
-          </li>
-          <li>
             <Link href="#">Enterprise</Link>
-          </li>
-        </ul>
+        </div>
       </nav>
       <aside className="flex items-center gap-4">
         <Link
