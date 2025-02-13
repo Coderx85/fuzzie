2c2d21d457e7d94389c41c143285cf833ef6ba68
Coderx85 <work.priyanshu085@gmail.com>
2025-02-14 01:50:26 +0530

    Update Workflow folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/providers/theme-provider'
-import { Toaster } from '@/components/ui/sonner'
-import ModalProvider from '@/providers/modal-provider'
 import { ClerkProvider } from '@clerk/nextjs'
+import ModalProvider from '@/providers/modal-provider'
+import { Toaster } from '@/components/ui/sonner'
 
 const font = DM_Sans({ subsets: ['latin'] })
 
@@ -19,8 +19,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <>
-      <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en">
         <body className={font.className}>
           <ThemeProvider
@@ -36,7 +37,6 @@ export default function RootLayout({
           </ThemeProvider>
         </body>
       </html>
-      </ClerkProvider>
-    </>
+    </ClerkProvider>
   )
 }
